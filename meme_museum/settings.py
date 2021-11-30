62089dd5dc0fc081137681ea93fb33ddbfe1045a
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2021-11-30 19:07:45 +0800

    rando experiment

--- meme_museum/settings.py
+++ meme_museum/settings.py
@@ -105,12 +105,12 @@ WSGI_APPLICATION = 'meme_museum.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -156,3 +156,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600,
+                                              ssl_require=True)
